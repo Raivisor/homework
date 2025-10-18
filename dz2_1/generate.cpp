8f43a3fa937a95cc202098a50bfacfaf543ff0c2
Raivisor <georgijsmirnov159@gmail.com>
2025-10-18 12:00:56 +0300

    applied the declaration/definition method

--- dz2_1/generate.cpp
+++ dz2_1/generate.cpp
@@ -3,7 +3,7 @@
 #include <chrono>
 #include <random>
 
-int* biv::generate(int* array, const int array_size){
+int* biv::generate(int* array, const int array_size) {
     std::mt19937 mt(time(nullptr));
 
     for(int i = 0; i < array_size; i++) {
--- dz2_1/io.cpp
+++ dz2_1/io.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-void biv::print_array(const char* const comment, int* array, const int array_size){
+void biv::print_array(const char* const comment, int* array, const int array_size) {
     const char space = ' ';
     std::cout << comment;
     for(int i = 0; i < array_size; i++) {
